77709057316541c750b2beeb976fcccfed695cac
Brian Sokhela <briankwazikwenkosi@gmail.com>
2025-10-05 21:50:37 +0200

    allowed-hosts

--- lumo/settings.py
+++ lumo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lumosoft.co.za', 'www.lumosoft.co.za', 'charismatic-dream.up.railway.app']
 
 
 # Application definition
